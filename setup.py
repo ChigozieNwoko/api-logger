e35e26a84de8269cf378e1eb38f62ee9aaebbbf2
ChigozieNwoko <Chigznwoko@gmail.com>
2022-06-14 19:05:46 +0100

    Cleanup commits

---
+++ setup.py
@@ -0,0 +1,42 @@
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+import setuptools
+
+
+def get_long_desc():
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+    return long_description
+
+
+setuptools.setup(
+    name="drf_api_logger",
+    version="1.1.10",
+    author="Charles Nwoko",
+    author_email="chigznwoko@gmail.com",
+    description="An API Logger for a Django Rest Framework project.",
+    long_description=get_long_desc(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Charles-Nwoko/DRF-API-Logger",
+    packages=setuptools.find_packages(),
+    install_requires=["djangorestframework>=3.7.4", "bleach>=3.1.5"],
+    license='MIT',
+    python_requires='>=3.6',
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        "Operating System :: OS Independent",
+    ],
+)
